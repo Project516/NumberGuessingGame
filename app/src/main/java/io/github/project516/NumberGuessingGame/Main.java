441437df66b42da03e865306ff748dfd15e8da1a
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-01 19:36:52 +0000

    add systeminfo class

--- app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
+++ app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
@@ -2,6 +2,8 @@ package io.github.project516.NumberGuessingGame;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        SystemInfo SysInfo = new SystemInfo();
+        System.out.println("Running on " + Runtime.version());
+        System.out.println(SysInfo.vendor());
     }
 }
