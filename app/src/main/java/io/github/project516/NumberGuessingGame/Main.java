8547934094818c1b547df3e9f85aed3c94e5558f
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-01 19:43:55 +0000

    debug text

--- app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
+++ app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
@@ -3,7 +3,8 @@ package io.github.project516.NumberGuessingGame;
 public class Main {
     public static void main(String[] args) {
         SystemInfo SysInfo = new SystemInfo();
-        System.out.println("Running on " + Runtime.version());
-        System.out.println(SysInfo.vendor());
+        System.out.println("Java version: " + Runtime.version());
+        System.out.println("Vendor: " + SysInfo.vendor());
+        System.out.println("JDK name: " + SysInfo.name());
     }
 }
