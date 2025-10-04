e4c6e4c1de46d0e1a9ba374279b4a0aadb21054d
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-04 21:13:45 +0000

    wip gui

---
+++ app/src/main/java/io/github/project516/NumberGuessingGame/GUI.java
@@ -0,0 +1,10 @@
+package io.github.project516.NumberGuessingGame;
+
+import javax.swing.*;
+
+public class GUI {
+    void createWindow() {
+        JFrame jframe = new JFrame();
+        jframe.setVisible(true);
+    }
+}
