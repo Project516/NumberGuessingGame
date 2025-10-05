b95e28042daf65793029fb9029b432fd5b176a7d
Project516 <Project516@users.noreply.github.com>
2025-10-05 16:42:56 +0000

    deploy: ad5a75757ddf9be9e9463756d8a42e3f8ad959df

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.project516.NumberGuessingGame","l":"Main"},{"p":"io.github.project516.NumberGuessingGame","l":"ScannerHelper"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"io.github.project516.NumberGuessingGame","l":"Main"},{"p":"io.github.project516.NumberGuessingGame","l":"ScannerHelper"}];updateSearchResults();
