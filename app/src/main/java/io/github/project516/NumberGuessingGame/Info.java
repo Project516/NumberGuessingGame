1deb3d01e32c84c9168e0de03963e0b765b02c3e
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-05 17:37:05 +0000

    fix javadoc, add blank classes

---
+++ app/src/main/java/io/github/project516/NumberGuessingGame/Info.java
@@ -0,0 +1,9 @@
+package io.github.project516.NumberGuessingGame;
+
+public class Info {
+
+    String gameInfo() {
+
+        return "This is a number guessing game.\nYou have to guess a number between 0 and 100.";
+    }
+}
