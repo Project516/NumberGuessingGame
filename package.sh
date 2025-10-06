5264208aa993280ccbd8005b2241aa637b4904da
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-06 13:30:25 +0000

    add license to releases

--- package.sh
+++ package.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+rm -rf NumberGuessingGame
 rm -rf temp
+rm -r archive.zip
 cd app
 gradle build
 cd ..
-mkdir temp
-cp -r app/build/libs/app.jar temp/app.jar
-cp -r scripts/run.bat temp/run.bat
-cp -r scripts/run.sh temp/run.sh
-cp -r README.md temp/README.txt
-zip -r archive.zip temp/
-rm -rf temp
+mkdir NumberGuessingGame
+cp -r app/build/libs/app.jar NumberGuessingGame/game.jar
+cp -r scripts/run.bat NumberGuessingGame/run.bat
+cp -r scripts/run.sh NumberGuessingGame/run.sh
+cp -r README.md NumberGuessingGame/README.txt
+cp -r LICENSE NumberGuessingGame/LICENSE
+zip -r archive.zip NumberGuessingGame/
+rm -rf NumberGuessingGame
