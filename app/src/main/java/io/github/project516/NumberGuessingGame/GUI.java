6c6c813e5b362cdae43dbcfe4ddb6cb150c2efe7
Spectrum3847 <spectrum3847@gmail.com>
2025-10-10 10:58:44 -0500

    GUI has a name
    Co-authored-by: Project516 <138796702+project516@users.noreply.github.com>

--- app/src/main/java/io/github/project516/NumberGuessingGame/GUI.java
+++ app/src/main/java/io/github/project516/NumberGuessingGame/GUI.java
@@ -1,14 +1,21 @@
 package io.github.project516.NumberGuessingGame;
 
+import java.awt.*;
 import javax.swing.*;
 
 // TODO make gui window
 
 public class GUI {
     void createWindow() {
-        JFrame jframe = new JFrame();
-        jframe.setVisible(true); // Set visible
+        JFrame jframe = new JFrame("Number Guessing Game");
+        JLabel jLabel = new JLabel("Number Guessing Game", SwingConstants.CENTER);
+
+        jframe.getContentPane().add(jLabel, BorderLayout.CENTER);
+
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Exit on close
+        jframe.setSize(300, 300);
+
+        jframe.setVisible(true); // Set visible
     }
 
     public static void main(String[] args) { // Test GUI
