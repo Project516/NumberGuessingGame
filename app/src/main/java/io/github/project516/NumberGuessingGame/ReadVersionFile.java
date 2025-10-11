4caa6b8240d83f9c11724f4b857204b7a31b57d8
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-11 20:45:07 +0000

    version file

---
+++ app/src/main/java/io/github/project516/NumberGuessingGame/ReadVersionFile.java
@@ -0,0 +1,8 @@
+package io.github.project516.NumberGuessingGame;
+
+public class ReadVersionFile {
+    // TODO - make this read the file in the resources directory
+    public String readVersion() {
+        return "rolling";
+    }
+}
