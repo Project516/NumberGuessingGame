9fb70812a70aff8519ad260a1dd2d6356098d467
eycastillo <138796702+Project516@users.noreply.github.com>
2025-10-10 19:26:26 -0500

    it works

--- package.bat
+++ package.bat
@@ -3,9 +3,6 @@
 rmdir /s /q NumberGuessingGame 2>nul
 rmdir /s /q temp 2>nul
 del /f archive.zip 2>nul
-cd app
-call gradle build
-cd ..
 mkdir NumberGuessingGame
 copy app\build\libs\app.jar NumberGuessingGame\game.jar
 copy scripts\run.bat NumberGuessingGame\run.bat
@@ -14,3 +11,5 @@ copy README.md NumberGuessingGame\README.txt
 copy LICENSE NumberGuessingGame\LICENSE
 powershell -command "Compress-Archive -Path NumberGuessingGame -DestinationPath archive.zip -Force"
 rmdir /s /q NumberGuessingGame
+
+@pause
