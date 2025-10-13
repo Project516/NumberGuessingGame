e7450262cba9950048d373ced63ffd1b67364746
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-13 18:12:23 +0000

    split game logic to sepret class

--- app/src/main/java/io/github/project516/NumberGuessingGame/GameLogic.java
+++ app/src/main/java/io/github/project516/NumberGuessingGame/GameLogic.java
@@ -2,6 +2,29 @@ package io.github.project516.NumberGuessingGame;
 
 public class GameLogic {
     // TODO port game logic from Main to GameLogic
-    void start() {}
-    
+    void game() {
+
+        RandomNumber ranNumber = new RandomNumber();
+        int number = ranNumber.number(100);
+        int numOfGuesses = 0;
+        ScannerHelper scan = new ScannerHelper();
+        CheckGuess check = new CheckGuess();
+
+        while (true) {
+
+            System.out.print("Guess a number: ");
+            int guess = scan.userGuess();
+            check.check(guess);
+            if (guess > number) {
+                System.out.println("You guessed to much!");
+            } else if (guess < number) {
+                System.out.println("You guessed to little!");
+            } else {
+                numOfGuesses++;
+                System.out.println("Took you " + numOfGuesses + " guesses!");
+                break;
+            }
+            numOfGuesses++;
+        }
+    }
 }
--- app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
+++ app/src/main/java/io/github/project516/NumberGuessingGame/Main.java
@@ -4,37 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int numOfGuesses = 0;
-
         ScannerHelper scan = new ScannerHelper();
-        RandomNumber ranNumber = new RandomNumber();
-        CheckGuess check = new CheckGuess();
         DebugInfo debugInfo = new DebugInfo();
         GameInfo gameInfo = new GameInfo();
+        GameLogic logic = new GameLogic();
 
         debugInfo.launchDebug();
 
         try {
-            int number = ranNumber.number(100);
+            
             gameInfo.about();
-
-            while (true) {
-
-                System.out.print("Guess a number: ");
-                int guess = scan.userGuess();
-                check.check(guess);
-                if (guess > number) {
-                    System.out.println("You guessed to much!");
-                } else if (guess < number) {
-                    System.out.println("You guessed to little!");
-                } else {
-                    numOfGuesses++;
-                    break;
-                }
-                numOfGuesses++;
-            }
-
-            System.out.println("Took you " + numOfGuesses + " guesses!");
+            logic.game();
 
         } catch (Exception e) {
 
